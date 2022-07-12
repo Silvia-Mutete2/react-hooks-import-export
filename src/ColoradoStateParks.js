a0dd7ca47458c27c7d7c7ffe9b4a89ab9bf3ae4f
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-07-12 18:20:47 +0300

    finished assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,27 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import { trees, wildlife } from "./parks/RockyMountain"
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+    <h1>Colorado State Parks!</h1>;
+    < MesaVerde/>
+
+    </div>
+  
+  )
 }
+console.log(trees)
+  wildlife()
+  
+  console.log(RMFunctions.trees)
+  RMFunctions.wildlife()
+  RMFunctions.elevation()
+  
+
+export default ColoradoStateParks;
